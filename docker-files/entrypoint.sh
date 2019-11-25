56ed32739b59765239228ddddc6272622e4030f8
Jacob Rief <jacob.rief@gmail.com>
2019-11-25 15:25:21 +0100

    restst with fallback-safe entrypoint

--- docker-files/entrypoint.sh
+++ docker-files/entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'testsuite' ]; then
-    exec pytest --capture=no tests
+if [ "X$1" == "Xtestsuite" ]; then
+	exec pytest --capture=no tests
 fi
 
-su django -c "python /web/manage.py migrate"
-exec uwsgi --ini uwsgi.ini "$@"
+manage_migrate() {
+	su django -c "python /web/manage.py migrate"
+}
+
+if [ "X$1" == "Xuwsgi" ]; then
+	manage_migrate
+	exec "$@" --ini uwsgi.ini
+elif [ "X$1" == "X" ]; then
+	manage_migrate
+	exec uwsgi --ini uwsgi.ini
+else
+	exec "$@"
+fi
